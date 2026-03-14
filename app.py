786b533a78282723e41f135d5a42e4b576e3fd66
hemminkn <nooralotta.hemminki@gmail.com>
2026-03-14 13:49:06 +0200

    added using the session

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, session
 from flask import redirect, render_template, request
 from werkzeug.security import generate_password_hash
 import db
+import config
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
@@ -30,3 +32,12 @@ def create():
         return "ERROR: username is taken"
 
     return "User created"
+
+@app.route("/page1")
+def page1():
+    session["test"] = "aybabtu"
+    return "Session set"
+
+@app.route("/page2")
+def page2():
+    return "Information about the session: " + session["test"]
