747b6cb469ca78dd150b4a0678b81a0a47b77a7c
hemminkn <nooralotta.hemminki@gmail.com>
2026-03-16 21:40:38 +0200

    Add new post form

--- schema.sql
+++ schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE posts (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	description TEXT,
+	user_id INTEGER REFERENCES users
+);
