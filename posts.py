db52028f2d20208ac4d54888bb4ddc20206fc8b2
hemminkn <nooralotta.hemminki@gmail.com>
2026-03-17 09:37:37 +0200

    Add posts module

---
+++ posts.py
@@ -0,0 +1,6 @@
+import db
+
+def add_post(title, description, user_id):
+    sql = """INSERT INTO posts (title, description, user_id)
+            VALUES (?, ?, ?)"""
+    db.execute(sql, [title, description, user_id])
