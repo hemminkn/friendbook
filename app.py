706cd7c7b524ca79ad8b51272dcab4ee6a70bf2f
hemminkn <nooralotta.hemminki@gmail.com>
2026-03-25 10:57:59 +0200

    Don't allow user to change classes

--- app.py
+++ app.py
@@ -61,11 +61,17 @@ def create_post():
         abort(403)
     user_id = session["user_id"]
 
+    all_classes = posts.get_all_classes()
+
     classes = []
     for entry in request.form.getlist("classes"):
         if entry:
-            parts = entry.split(":")
-            classes.append((parts[0], parts[1]))
+            class_title, class_value = entry.split(":")
+            if class_title not in all_classes:
+                abort(403)
+            if class_value not in all_classes[class_title]:
+                abort(403)
+            classes.append((class_title, class_value))
 
     posts.add_post(title, description, user_id, classes)
 
@@ -109,8 +115,12 @@ def update_post():
     classes = []
     for entry in request.form.getlist("classes"):
         if entry:
-            parts = entry.split(":")
-            classes.append((parts[0], parts[1]))
+            class_title, class_value = entry.split(":")
+            if class_title not in all_classes:
+                abort(403)
+            if class_value not in all_classes[class_title]:
+                abort(403)
+            classes.append((class_title, class_value))
 
     posts.update_post(post_id, title, description, classes)
     
