3ead6f58dbd6ac4de93c7c20ff2c686f82181ed7
hemminkn <nooralotta.hemminki@gmail.com>
2026-04-13 13:22:47 +0300

    Added more classes

--- init.sql
+++ init.sql
@@ -1,5 +1,15 @@
 DELETE FROM classes;
 
-INSERT INTO classes (title, value) VALUES ("Type", "Friend");
-INSERT INTO classes (title, value) VALUES ("Type", "Close friend");
-INSERT INTO classes (title, value) VALUES ("Type", "Best friend");
+INSERT INTO classes (title, value) VALUES ("Closeness", "Friend");
+INSERT INTO classes (title, value) VALUES ("Closeness", "Close friend");
+INSERT INTO classes (title, value) VALUES ("Closeness", "Best friend");
+
+INSERT INTO classes (title, value) VALUES ("Random", "Situationship");
+INSERT INTO classes (title, value) VALUES ("Random", "Work bestie");
+INSERT INTO classes (title, value) VALUES ("Random", "Work Wife");
+INSERT INTO classes (title, value) VALUES ("Random", "Work husband");
+INSERT INTO classes (title, value) VALUES ("Random", "Day one");
+INSERT INTO classes (title, value) VALUES ("Random", "Secret sauce");
+INSERT INTO classes (title, value) VALUES ("Random", "Partner in slime");
+INSERT INTO classes (title, value) VALUES ("Random", "Ride or die");
+INSERT INTO classes (title, value) VALUES ("Random", "Chosen family");
